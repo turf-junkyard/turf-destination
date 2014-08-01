9e0d33ab785238c9750c0534546a0ca3ab8244f5
Jordan Rousseau <jrousseau@wdtinc.com>
2014-08-01 16:46:27 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+turf-destination
+=============
+
+Calculates the destination point given one point feature, distance in degrees, radians, miles, or kilometers, and bearing in degrees. This uses the haversine formula to account for global curvature.
+
+###Install
+
+```sh
+npm install turf-destination
+```
+
+###Parameters
+
+|name|description|
+|---|---|
+|point1|point feature|
+|distance|point feature|
+|bearing|number (-180 - 180)|
+|units|'miles' or 'kilometers'|
+
+###Usage
+
+```js
+destination(point1, distance, bearing, units)
+```
+
+###Example
+
+```javascript
+var destination = require('turf-destination')
+var point = require('turf-point')
+
+var point1 = point(-75.343, 39.984)
+var distance = 100
+var bearing = 90
+var units = 'miles' // or 'kilometers', 'degrees', 'radians'
+
+var destination = destination(point1, distance, bearing, units)
+
+console.log(destination)
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+//http://en.wikipedia.org/wiki/Haversine_formula
+//http://www.movable-type.co.uk/scripts/latlong.html
+var point = require('turf-point')
+
+module.exports = function (point1, distance, bearing, units) {
+    var coordinates1 = point1.geometry.coordinates
+    var longitude1 = toRad(coordinates1[0])
+    var latitude1 = toRad(coordinates1[1])
+
+    var R = 0
+    switch (units) {
+    case 'miles':
+        R = 3960
+        break
+    case 'kilometers':
+        R = 6373
+        break
+    case 'degrees':
+        R = 57.2957795
+        break
+    case 'radians':
+        R = 1
+        break
+    }
+
+    var latitude2 = Math.asin(Math.sin(latitude1) * Math.cos(distance / R) +
+        Math.cos(latitude1) * Math.sin(distance / R) * Math.cos(bearing));
+    var longitude2 = longitude1 + Math.atan2(Math.sin(bearing) * Math.sin(distance / R) * Math.cos(latitude1),
+        Math.cos(distance / R) - Math.sin(latitude1) * Math.sin(latitude2));
+
+    return point(toDeg(longitude2), toDeg(latitude2))
+};
+
+function toRad(degree) {
+    return degree * Math.PI / 180
+}
+
+function toDeg(rad) {
+    return rad * 180 / Math.PI
+}
