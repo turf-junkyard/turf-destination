faa387528ebde8c45fe8317511e73e3b392c5407
morganherlocker <morgan.herlocker@gmail.com>
2014-12-11 15:27:12 -0500

    styles

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 //http://en.wikipedia.org/wiki/Haversine_formula
 //http://www.movable-type.co.uk/scripts/latlong.html
-var point = require('turf-point')
+var point = require('turf-point');
 
 module.exports = function (point1, distance, bearing, units) {
-    var coordinates1 = point1.geometry.coordinates
-    var longitude1 = toRad(coordinates1[0])
-    var latitude1 = toRad(coordinates1[1])
-    var bearing_rad = toRad(bearing)
+    var coordinates1 = point1.geometry.coordinates;
+    var longitude1 = toRad(coordinates1[0]);
+    var latitude1 = toRad(coordinates1[1]);
+    var bearing_rad = toRad(bearing);
 
-    var R = 0
+    var R = 0;
     switch (units) {
     case 'miles':
-        R = 3960
+        R = 3960;
         break
     case 'kilometers':
-        R = 6373
+        R = 6373;
         break
     case 'degrees':
-        R = 57.2957795
+        R = 57.2957795;
         break
     case 'radians':
-        R = 1
+        R = 1;
         break
     }
 
@@ -29,13 +29,13 @@ module.exports = function (point1, distance, bearing, units) {
     var longitude2 = longitude1 + Math.atan2(Math.sin(bearing_rad) * Math.sin(distance / R) * Math.cos(latitude1),
         Math.cos(distance / R) - Math.sin(latitude1) * Math.sin(latitude2));
 
-    return point(toDeg(longitude2), toDeg(latitude2))
+    return point(toDeg(longitude2), toDeg(latitude2));
 };
 
 function toRad(degree) {
-    return degree * Math.PI / 180
+    return degree * Math.PI / 180;
 }
 
 function toDeg(rad) {
-    return rad * 180 / Math.PI
+    return rad * 180 / Math.PI;
 }
