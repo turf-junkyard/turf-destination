4c9fd7ab4b933e45a93c0857abd8f7237ddddda8
Tim Channell <tim.channell@gmail.com>
2016-01-06 15:40:15 -0500

    bump to 1.3.0, eslint

--- index.js
+++ index.js
@@ -38,40 +38,40 @@ var point = require('turf-point');
  * //=result
  */
 module.exports = function (point1, distance, bearing, units) {
-    var coordinates1 = point1.geometry.coordinates;
-    var longitude1 = toRad(coordinates1[0]);
-    var latitude1 = toRad(coordinates1[1]);
-    var bearing_rad = toRad(bearing);
+  var coordinates1 = point1.geometry.coordinates;
+  var longitude1 = toRad(coordinates1[0]);
+  var latitude1 = toRad(coordinates1[1]);
+  var bearingRad = toRad(bearing);
 
-    var R = 0;
-    switch (units) {
+  var R = 0;
+  switch (units) {
     case 'miles':
-        R = 3960;
-        break;
+      R = 3960;
+      break;
     case 'kilometers':
     case 'kilometres':
-        R = 6373;
-        break;
+      R = 6373;
+      break;
     case 'degrees':
-        R = 57.2957795;
-        break;
+      R = 57.2957795;
+      break;
     case 'radians':
-        R = 1;
-        break;
-    }
+      R = 1;
+      break;
+  }
 
-    var latitude2 = Math.asin(Math.sin(latitude1) * Math.cos(distance / R) +
-        Math.cos(latitude1) * Math.sin(distance / R) * Math.cos(bearing_rad));
-    var longitude2 = longitude1 + Math.atan2(Math.sin(bearing_rad) * Math.sin(distance / R) * Math.cos(latitude1),
-        Math.cos(distance / R) - Math.sin(latitude1) * Math.sin(latitude2));
+  var latitude2 = Math.asin(Math.sin(latitude1) * Math.cos(distance / R) +
+      Math.cos(latitude1) * Math.sin(distance / R) * Math.cos(bearingRad));
+  var longitude2 = longitude1 + Math.atan2(Math.sin(bearingRad) * Math.sin(distance / R) * Math.cos(latitude1),
+      Math.cos(distance / R) - Math.sin(latitude1) * Math.sin(latitude2));
 
-    return point([toDeg(longitude2), toDeg(latitude2)]);
+  return point([toDeg(longitude2), toDeg(latitude2)]);
 };
 
 function toRad(degree) {
-    return degree * Math.PI / 180;
+  return degree * Math.PI / 180;
 }
 
 function toDeg(rad) {
-    return rad * 180 / Math.PI;
+  return rad * 180 / Math.PI;
 }
